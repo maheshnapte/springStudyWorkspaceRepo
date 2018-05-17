2a73f079939cb4d2983ea9799d9cc1a52b708139
Mahesh Napte <memaheshnapte@gmail.com>
2018-05-17 08:03:17 +0530

    commit and push

--- DIbysettermethod1/src/com/javatpoint/Employee.java
+++ DIbysettermethod1/src/com/javatpoint/Employee.java
@@ -1,31 +1,39 @@
 package com.javatpoint;
 
 public class Employee {
-private int id;
-private String name;
-private String city;
+	private int id;
+	private String name;
+	private String city;
 
-public int getId() {
-	return id;
-}
-public void setId(int id) {
-	this.id = id;
-}
-public String getName() {
-	return name;
-}
-public void setName(String name) {
-	this.name = name;
-}
+	public int getId() {
+		return id;
+	}
 
-public String getCity() {
-	return city;
-}
-public void setCity(String city) {
-	this.city = city;
-}
-void display(){
-	System.out.println(id+" "+name+" "+city);
-}
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	void display() {
+		System.out.println(id + " " + name + " " + city);
+
+		
+		
+	}
 
 }
